952ec65ccd3d437b1e600b346da38ad5fe1c260e
Hlupik77 <hlupik77@gmail.com>
2020-04-10 01:02:55 +0300

    progress animation does not work

--- js/script.js
+++ js/script.js
@@ -15,13 +15,13 @@ const reactBar = document.querySelector('.bar-react')
 var t1 = new TimelineLite()
 
 t1.fromTo(htmlBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
-    .fromTo(css.Bar, .75, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeOut})
+    .fromTo(cssBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeOut})
     .fromTo(jsBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(75% - 6px)`, ease: Power4.easeOut})
     .fromTo(reactBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(70% - 6px)`, ease: Power4.easeOut})
 
 
 const controller = new ScrollMagic.Controller()
-const scene = new ScrollMagic.Sense({
+const scene = new ScrollMagic.Scene({
     triggerElement: '.skills',
     triggerHook: 0
 })
@@ -42,7 +42,7 @@ const showRequiredCategory = event => {
     const getCategory = document.querySelector(`.category-${getId}`)
     const categories = document.querySelectorAll('div[class ^= "category-"]')
     for(i=0; i<categories.length; i++) {
-        if(categories[i].hasAttribute('class')){
+        if(categories[i].hasAttribute('class')) {
             categories[i].classList.remove('showCategory')
             categories[i].classList.add('hideCategory')
         }
